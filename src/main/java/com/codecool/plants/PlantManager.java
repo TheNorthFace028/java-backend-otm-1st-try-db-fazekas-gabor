8e8a0a69c3bbc3493d6888f818e2e7242b136aa9
Viking <gabor.fazekas028@gmail.com>
2022-11-02 16:36:35 +0100

    Add getPlantsToBeWatered test final commit

--- src/main/java/com/codecool/plants/PlantManager.java
+++ src/main/java/com/codecool/plants/PlantManager.java
@@ -1,5 +1,14 @@
 package com.codecool.plants;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.sql.DriverManager.getConnection;
+
 public class PlantManager {
 
     private final String dbUrl;
@@ -10,5 +19,20 @@ public class PlantManager {
         this.dbUrl = dbUrl;
         this.dbUser = dbUser;
         this.dbPassword = dbPassword;
+    }
+    public List<String> getPlantsToBeWatered() {
+        List<String> result = new ArrayList<>();
+        try (Connection conn = getConnection(dbUrl,dbUser, dbPassword)) {
+            String SQL = "SELECT DISTINCT p.plant_name FROM plants as p LEFT JOIN watered as w ON p.plant_id = w.plant_id WHERE w.watered_at < '2022-01-01' ORDER BY p.plant_name ASC";
+            PreparedStatement st = conn.prepareStatement(SQL);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()){
+                String name = rs.getString("plant_name");
+                result.add(name);
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return result;
     }
 }
